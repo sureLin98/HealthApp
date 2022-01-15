f56c73f6eb50d676cefccd26e1fb191fb8e4e9b4
LinShengGui <2664039714@qq.com>
2022-01-15 12:21:56 +0800

    初次提交，增加蓝牙串口功能

---
+++ app/src/main/java/com/android/healthapp/MainActivity.java
@@ -0,0 +1,328 @@
+package com.android.healthapp;
+
+import android.annotation.SuppressLint;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.android.healthapp.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,BLESPPUtils.OnBluetoothAction {
+
+    private ActivityMainBinding binding;
+    BLESPPUtils mBLESPPUtils;
+    // 保存搜索到的设备，避免重复
+    private ArrayList<BluetoothDevice> mDevicesList = new ArrayList<>();
+    DeviceDialogCtrl mDeviceDialogCtrl;
+    TextView mLogTv;
+    String dataStr;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initPermissions();
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBLESPPUtils.onDestroy();
+    }
+
+    /**
+     * 替换Fragment
+     */
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //transaction.replace(R.id.frame_layout,fragment);
+        transaction.commit();
+    }
+
+    /**
+     * 申请运行时权限，不授予会搜索不到设备
+     */
+    private void initPermissions() {
+        if (ContextCompat.checkSelfPermission(this, "android.permission-group.LOCATION") != 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{
+                            "android.permission.ACCESS_FINE_LOCATION",
+                            "android.permission.ACCESS_COARSE_LOCATION",
+                            "android.permission.ACCESS_WIFI_STATE"},
+                    1
+            );
+        }
+    }
+
+    /**
+     * 当发现新设备
+     *
+     * @param device 设备
+     */
+    @Override
+    public void onFoundDevice(BluetoothDevice device) {
+        // 判断是不是重复的
+        for (int i = 0; i < mDevicesList.size(); i++) {
+            if (mDevicesList.get(i).getAddress().equals(device.getAddress())) return;
+        }
+        // 添加，下次有就不显示了
+        mDevicesList.add(device);
+        // 添加条目到 UI 并设置点击事件
+        mDeviceDialogCtrl.addDevice(device, new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                BluetoothDevice clickDevice = (BluetoothDevice) v.getTag();
+                mBLESPPUtils.connect(clickDevice);
+            }
+        });
+    }
+
+    /**
+     * 当连接成功
+     *
+     * @param device 设备
+     */
+    @Override
+    public void onConnectSuccess(final BluetoothDevice device) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this,"连接到"+device.getName(),Toast.LENGTH_LONG).show();
+            }
+        });
+        mDeviceDialogCtrl.dismiss();
+    }
+
+    /**
+     * 当连接失败
+     *
+     * @param msg 失败信息
+     */
+    @Override
+    public void onConnectFailed(final String msg) {
+
+    }
+
+    /**
+     * 当接收到 byte 数组
+     *
+     * @param bytes 内容
+     */
+    @Override
+    public void onReceiveBytes(final byte[] bytes) {
+        dataStr = new String(bytes);
+        Log.d("data", dataStr);
+    }
+
+    /**
+     * 当调用接口发送 byte 数组
+     *
+     * @param bytes 内容
+     */
+    @Override
+    public void onSendBytes(final byte[] bytes) {
+
+    }
+
+    /**
+     * 当结束搜索设备
+     */
+    @Override
+    public void onFinishFoundDevice() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.titke_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.connect_bt:
+                // 初始化
+                mBLESPPUtils = new BLESPPUtils(this, this);
+                // 启用日志输出
+                mBLESPPUtils.enableBluetooth();
+                // 设置接收停止标志位字符串
+                mBLESPPUtils.setStopString("\r\n");
+                // 用户没有开启蓝牙的话打开蓝牙
+                if (!mBLESPPUtils.isBluetoothEnable()) mBLESPPUtils.enableBluetooth();
+                // 启动工具类
+                mBLESPPUtils.onCreate();
+
+                mDeviceDialogCtrl = new DeviceDialogCtrl(this);
+                mDeviceDialogCtrl.show();
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+    /**
+     * 设备选择对话框控制
+     */
+    private class DeviceDialogCtrl {
+        private LinearLayout mDialogRootView;
+        private ProgressBar mProgressBar;
+        private AlertDialog mConnectDeviceDialog;
+
+        DeviceDialogCtrl(Context context) {
+            // 搜索进度条
+            mProgressBar = new ProgressBar(context, null, android.R.attr.progressBarStyleHorizontal);
+            mProgressBar.setLayoutParams(
+                    new LinearLayout.LayoutParams(
+                            LinearLayout.LayoutParams.MATCH_PARENT,
+                            50
+                    )
+            );
+
+            // 根布局
+            mDialogRootView = new LinearLayout(context);
+            mDialogRootView.setOrientation(LinearLayout.VERTICAL);
+            mDialogRootView.addView(mProgressBar);
+            mDialogRootView.setMinimumHeight(700);
+
+            // 容器布局
+            ScrollView scrollView = new ScrollView(context);
+            scrollView.addView(mDialogRootView,
+                    new FrameLayout.LayoutParams(
+                            FrameLayout.LayoutParams.MATCH_PARENT,
+                            700
+                    )
+            );
+
+            // 构建对话框
+            mConnectDeviceDialog = new AlertDialog
+                    .Builder(context)
+                    .setNegativeButton("刷新", null)
+                    .setPositiveButton("取消", null)
+                    .create();
+            mConnectDeviceDialog.setTitle("选择连接的蓝牙设备");
+            mConnectDeviceDialog.setView(scrollView);
+            mConnectDeviceDialog.setCancelable(false);
+        }
+
+        /**
+         * 显示并开始搜索设备
+         */
+        void show() {
+            mBLESPPUtils.startDiscovery();
+            mConnectDeviceDialog.show();
+            mConnectDeviceDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    mConnectDeviceDialog.dismiss();
+                    return false;
+                }
+            });
+            mConnectDeviceDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mConnectDeviceDialog.dismiss();
+                    //finish();
+                }
+            });
+            mConnectDeviceDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mDialogRootView.removeAllViews();
+                    mDialogRootView.addView(mProgressBar);
+                    mDevicesList.clear();
+                    mBLESPPUtils.startDiscovery();
+                }
+            });
+        }
+
+        /**
+         * 取消对话框
+         */
+        void dismiss() {
+            mConnectDeviceDialog.dismiss();
+        }
+
+        /**
+         * 添加一个设备到列表
+         * @param device 设备
+         * @param onClickListener 点击回调
+         */
+        private void addDevice(final BluetoothDevice device, final View.OnClickListener onClickListener) {
+            runOnUiThread(new Runnable() {
+                @SuppressLint("SetTextI18n")
+                @Override
+                public void run() {
+                    TextView devTag = new TextView(MainActivity.this);
+                    devTag.setClickable(true);
+                    devTag.setPadding(20,20,20,20);
+                    devTag.setBackgroundResource(R.drawable.rect_round_button_ripple);
+                    devTag.setText(device.getName() + "\nMAC:" + device.getAddress());
+                    devTag.setTextColor(Color.WHITE);
+                    devTag.setOnClickListener(onClickListener);
+                    devTag.setTag(device);
+                    devTag.setLayoutParams(
+                            new LinearLayout.LayoutParams(
+                                    LinearLayout.LayoutParams.MATCH_PARENT,
+                                    LinearLayout.LayoutParams.WRAP_CONTENT
+                            )
+                    );
+                    ((LinearLayout.LayoutParams) devTag.getLayoutParams()).setMargins(
+                            20, 20, 20, 20);
+                    mDialogRootView.addView(devTag);
+                }
+            });
+        }
+    }
+
+}
